caf1b3ab9808726000062db741cb05a426957522
Poupav <thanachot10072550@gmail.com>
2025-04-22 00:21:03 +0700

    Fix

--- Assets/Poupav/Script/DimensionManager.cs
+++ Assets/Poupav/Script/DimensionManager.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class DimensionManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public CameraFollow cameraFollow;
+    public Transform cameraTransform;
 
-    // Update is called once per frame
+    public Quaternion rotation3D = Quaternion.Euler(30f, 45f, 0f);
+    public Quaternion rotation2D = Quaternion.Euler(90f, 0f, 0f);
+
+    public float rotationDuration = 1f;
+
+    private bool is2D = false;
+    private bool isRotating = false;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && !isRotating)
+        {
+            is2D = !is2D;
+
+            // Tell camera follow script to switch offset
+            cameraFollow.is2D = is2D;
+
+            // Start smooth rotation
+            StartCoroutine(SmoothRotate(is2D ? rotation2D : rotation3D));
+        }
+    }
+
+    System.Collections.IEnumerator SmoothRotate(Quaternion targetRotation)
+    {
+        isRotating = true;
+
+        Quaternion startRotation = cameraTransform.rotation;
+        float elapsed = 0f;
+
+        while (elapsed < rotationDuration)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsed / rotationDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        cameraTransform.rotation = targetRotation;
+        isRotating = false;
     }
 }
