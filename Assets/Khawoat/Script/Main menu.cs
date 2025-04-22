bad6ca5b9b1aa4af4a0a11d3483aaeff724a50a6
Khawoat99 <Kowinninetynine034@gmail.com>
2025-04-22 13:12:28 +0700

    UI

---
+++ Assets/Khawoat/Script/Main menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenuPanel;     
+    public GameObject settingsPanel;     
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
